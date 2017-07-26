c43c82ab724a0cef96c15421828d900434ec7e01
俞益 <yuyi@yuyideMacBook-Pro.local>
2017-07-26 09:44:22 +0800

    Initial Commit

---
+++ 天气助手/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  天气助手
+//
+//  Created by 俞益 on 2017/7/26.
+//  Copyright © 2017年 俞益. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
